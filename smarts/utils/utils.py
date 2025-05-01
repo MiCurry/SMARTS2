f1847996538580ac8941c3b1f6f0dd5c77fba01c
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-05-01 16:41:01 -0600

    Add all_matches utils to help with finding execuatables

--- smarts/utils/utils.py
+++ smarts/utils/utils.py
@@ -13,3 +13,33 @@ def save_run_command(fname: str, cmd: List[str]):
 
      
     return True
+
+def matches_in_str(string: str, matches: List[str], discards=None) -> bool:
+    """ Return True, if all matches are in string, otherwise return False. 
+
+
+    discards (optional) - any matches in discard will return False
+    """
+    for match in matches:
+        if match not in string:
+            return False
+
+    for d in discards:
+        if d in string:
+            return False
+
+    return True
+
+def all_matches(strs: List[str], matches: List[str], discards=None) -> List[str]:
+    """ Given a list of strings, return the strings that match all the strings in matches. 
+
+    If the strs contain any matches to discard (optional), then that string will be discarded.
+    """
+
+    matched = []
+    for f in strs:
+        if matches_in_str(f, matches, discards):
+            matched.append(f)
+
+
+    return matched
