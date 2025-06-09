e9abf0f14068b54998a4591f1ee29f5e019742df
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-06-09 15:28:35 -0600

    Add utilities to help with process ModEM matlab data

---
+++ smarts/utils/ModEM_Utils.py
@@ -0,0 +1,82 @@
+
+
+
+from smarts.utils import matlab_utils
+
+def convert_ml_info_to_py(eng, obj: str) -> list[dict]:
+    infos = []
+    info_size = eng.eval(f'size({obj})')[0]
+    x = int(info_size[0])
+    y = int(info_size[1])
+
+    print('Info Size:', info_size)
+    for i in range(1, y+1):
+        access_str = f'{obj}{{{i}}}'
+
+        infos.append({
+            'data' : eng.eval(f'{access_str}.data'),
+            'err' : eng.eval(f'{access_str}.err'),
+            'lat' : eng.eval(f'{access_str}.lat'), 
+            'lon' : eng.eval(f'{access_str}.lon'), 
+            'loc' : eng.eval(f'{access_str}.loc'), 
+            'code' : matlab_utils.ml_n2m_to_pylist(eng, f'{access_str}.code'),
+            'per' : eng.eval(f'{access_str}.per'), 
+            'ncomp' : eng.eval(f'{access_str}.ncomp'),
+            'comp' : matlab_utils.ml_n2m_to_pylist(eng, f'{access_str}.comp')
+        })
+
+    return infos
+
+def convert_ml_data_to_py(eng, obj: str) -> list[dict]:
+    data_size = eng.eval(f'size({obj})')[0]
+    x = int(data_size[0])
+    y = int(data_size[1])
+
+    print(data_size, x, y)
+
+
+    allData = []
+    for i in range(1, y + 1):
+        # For format strings, you can escape {} by {{}}.
+        # Here we need to escape the first set of curley braces for matlab: data{}
+        # then we need to use another set of {} to access the specific: i.e. data{i}
+        access_str = f'{obj}{{{i}}}'
+
+        t = eng.eval(f'{access_str}.T')
+        cmplx = eng.eval(f'{access_str}.Cmplx')
+        units = eng.eval(f'{access_str}.units')
+        signConvention = eng.eval(f'{access_str}.signConvention')
+        nComp = eng.eval(f'{access_str}.nComp')
+        siteLoc = eng.eval(f'{access_str}.siteLoc')
+        siteChar = eng.eval(f'{access_str}.siteChar')
+        Z = eng.eval(f'{access_str}.Z')
+        Zerr = eng.eval(f'{access_str}.Zerr')
+        origin = eng.eval(f'{access_str}.origin')
+        orient = eng.eval(f'{access_str}.orient')
+        lat = eng.eval(f'{access_str}.lat')
+        lon = eng.eval(f'{access_str}.lon')
+        compChar = matlab_utils.ml_n2m_to_pylist(eng, f'{access_str}.compChar') 
+
+        data = {'T' : t,
+                'Cmplx' : cmplx,
+                'units' : units,
+                'signConvention' : signConvention,
+                'nComp' : nComp,
+                'siteLoc' : siteLoc,
+                'siteChar' : siteChar,
+                'Z' : Z,
+                'Zerr' : Zerr,
+                'origin' : origin,
+                'orient' : orient,
+                'lat' : lat,
+                'lon' : lon,
+                'compChar' : compChar}
+
+        allData.append(data)
+
+    return allData
+
+
+class ModEM_Data:
+    def __init__(self, eng):
+        self.eng = eng
